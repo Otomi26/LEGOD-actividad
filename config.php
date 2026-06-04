97b1a67c5190bafdfa378c6b44fb9b57b1e1c4cc
Otomi26 <324490512@alumno.enp.unam.mx>
2026-06-04 14:35:48 -0600

    docs(php): conectar con base de datos

--- config.php
+++ config.php
@@ -1,11 +1,21 @@
 <?php    
-    const DBHOST = "";
-    const DBUSER = "";
-    const PASSWORD = "";
-    const DB = "";
+    const DBHOST = "localhost";//RAIZ
+    const DBUSER = "root";// usuario
+    const PASSWORD = "";//poner contraseña si existe
+    const DB = "lego";
 
     function connect()
     {
+        $conexion = mysqli_connect(DBHOST, DBUSER, PASSWORD, DB);// conecta base de datos con la pagina
+        /*
+        --PASO : var_dump de $conexion:
+        --PASO: regresar variable para un archivo externo
+        */
+        return $conexion;
         
     }
+    // --PASO : llamar a la funcion antes de pasarla a otro archivo
+    $conexion = connect(); //llama a la funcion para que se ejecute
+
+
 ?>
